fd3445e7ac42c39bac257b73c434ce7fb35e2530
ldyxs <774911187@qq.com>
2016-09-18 21:51:23 +0800

    new file 01

---
+++ src/com/atguigu/HelloWord.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello word");
+		System.out.println("hello word 01");
+		
+	}
+	
+	
+}
