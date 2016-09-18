1c85b3be497d8bb8b222bc3df7b5da0adc2449cf
ldyxs <774911187@qq.com>
2016-09-19 00:28:18 +0800

    new file 04ddd

--- src/com/atguigu/HelloWord.java
+++ src/com/atguigu/HelloWord.java
@@ -7,6 +7,10 @@ public class HelloWord {
 		System.out.println("hello word");
 		System.out.println("hello word 01");
 		System.out.println("服务器更新了");
+		System.out.println("aaaa");
+		System.out.println("bbbb");
+		System.out.println("cccc");
+		
 		System.out.println("=================");
 	}
 	
